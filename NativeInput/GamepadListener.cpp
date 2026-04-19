321744e54ff90e000e389e2d6185dc9ecfd7c6f6
scruboptimal <scruboptimal@users.noreply.github.com>
2026-04-19 11:52:43 -0700

    initial project setup

---
+++ NativeInput/GamepadListener.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "GamepadListener.h"
+#if __has_include("GamepadListener.g.cpp")
+#include "GamepadListener.g.cpp"
+#endif
+
+namespace winrt::NativeInput::implementation
+{
+    int32_t GamepadListener::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void GamepadListener::MyProperty(int32_t /*value*/)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ NativeInput/GamepadListener.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "GamepadListener.g.h"
+
+namespace winrt::NativeInput::implementation
+{
+    struct GamepadListener : GamepadListenerT<GamepadListener>
+    {
+        GamepadListener() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::NativeInput::factory_implementation
+{
+    struct GamepadListener : GamepadListenerT<GamepadListener, implementation::GamepadListener>
+    {
+    };
+}
