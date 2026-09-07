aef5d3c6052dbdb4d09917e94b5223d2ecfac864
scruboptimal <scruboptimal@users.noreply.github.com>
2026-09-07 11:10:06 -0700

    WIP

---
+++ VirtualControllerWpf/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VirtualControllerWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VirtualController? virtualController;
+        private Window? controllerWindow;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenVirtualController_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.controllerWindow != null)
+            {
+                this.controllerWindow.Close();
+            }
+
+            try
+            {
+                OpenFileDialog picker = new OpenFileDialog()
+                {
+                    Filter = "SVG files (*.svg)|*.svg|All files (*.*)|*.*",
+                };
+
+                bool? result = picker.ShowDialog();
+                if (result != true)
+                {
+                    throw new Exception("No svg file selected");
+                }
+
+                this.virtualController = new VirtualController(picker.FileName);
+                this.controllerWindow = new Window()
+                {
+                    Content = this.virtualController.Canvas,
+                    Title = "Virtual Controller",
+                    SizeToContent = SizeToContent.WidthAndHeight,
+                };
+
+                this.controllerWindow.Activate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred creating the virtual controller: {ex.Message}");
+            }
+        }
+    }
+}
