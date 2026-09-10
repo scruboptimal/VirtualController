c85dcd7955a87951e45b9e5e6e916afae1efec0d
scruboptimal <scruboptimal@users.noreply.github.com>
2026-09-10 00:54:37 -0700

    Cleanup dead projects

--- NativeInput/pch.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include <unknwn.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
-
-#include <wil/result_macros.h>
-#include <wrl/client.h>
-using namespace Microsoft::WRL;
